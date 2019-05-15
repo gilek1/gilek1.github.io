ee5b7fce28a23c03a2cc8bf6987f0e56fe5304c3
gilek1 <36971368+gilek1@users.noreply.github.com>
2019-05-15 22:02:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function checkLoginState() {
   });
 }
 function testAPI() {
-  FB.api("/me?fields=name,email", function(response) {
+  FB.api("/me?fields=name,email,picture{url}", function(response) {
     if (response && !response.error) {
       buildProfile(response);
     }
@@ -45,7 +45,7 @@ function buildProfile(user) {
   <h3>Cześć ${user.name}!</h3>
   <h3>Email: ${user.email}</h3>
   <h3>ID: ${user.id}</h3>
-  
+  <img src="${user.picture.data.url}">
      
 `;
   document.getElementById("profile").innerHTML = profile;
